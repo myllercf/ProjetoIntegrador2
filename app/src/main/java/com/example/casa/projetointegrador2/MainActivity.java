cd3ab61b32ab3a8b0d8014d3913088b94c51fe32
Myller Claudino de Freitas <myllercf@gmail.com>
2017-04-12 00:02:18 -0300

    aviso de saude, troca de laco condicional e site do ministerio da saude

--- app/src/main/java/com/example/casa/projetointegrador2/MainActivity.java
+++ app/src/main/java/com/example/casa/projetointegrador2/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "myllercf@gmail.com", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "http://portalsaude.saude.gov.br", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- app/src/main/java/com/example/casa/projetointegrador2/Resultado.java
+++ app/src/main/java/com/example/casa/projetointegrador2/Resultado.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -44,35 +45,39 @@ public class Resultado extends Activity {
 
                 final ImageView imgTabela = (ImageView) findViewById(R.id.tabela);
                 imgTabela.setImageResource(imagens[sexo]);
+
+                Toast.makeText(Resultado.this, "Para maiores informações o médico deverá ser consultado.", Toast.LENGTH_LONG).show();
             }
         }
     }
 
     private void qualCategoria(Float imc, int sexo) {
-        if (sexo == 0){
-            if (imc < 20.7){
-                cateoria = "Abaixo do peso";
-            } else if (imc >= 20.7 && imc <= 26.4){
-                cateoria = "Peso ideal";
-            } else if (imc >= 26.5 && imc <= 27.8){
-                cateoria = "Pouco acima do peso";
-            } else if (imc >= 27.9 && imc <= 31.1){
-                cateoria = "Acima do peso";
-            } else if (imc >= 31.2){
-                cateoria = "Obesidade";
-            }
-        } else {
-            if (imc < 19.1){
-                cateoria = "Abaixo do peso";
-            } else if (imc >= 19.1 && imc <= 25.8){
-                cateoria = "Peso ideal";
-            } else if (imc >= 25.9 && imc <= 27.3){
-                cateoria = "Pouco acima do peso";
-            } else if (imc >= 27.4 && imc <= 32.3){
-                cateoria = "Acima do peso";
-            } else if (imc >= 32.4){
-                cateoria = "Obesidade";
-            }
+        switch (sexo){
+            case 0:
+                if (imc < 20.7){
+                    cateoria = "Abaixo do peso";
+                } else if (imc >= 20.7 && imc <= 26.4){
+                    cateoria = "Peso ideal";
+                } else if (imc >= 26.5 && imc <= 27.8){
+                    cateoria = "Pouco acima do peso";
+                } else if (imc >= 27.9 && imc <= 31.1){
+                    cateoria = "Acima do peso";
+                } else if (imc >= 31.2){
+                    cateoria = "Obesidade";
+                }
+
+            case 1:
+                if (imc < 19.1){
+                    cateoria = "Abaixo do peso";
+                } else if (imc >= 19.1 && imc <= 25.8){
+                    cateoria = "Peso ideal";
+                } else if (imc >= 25.9 && imc <= 27.3){
+                    cateoria = "Pouco acima do peso";
+                } else if (imc >= 27.4 && imc <= 32.3){
+                    cateoria = "Acima do peso";
+                } else if (imc >= 32.4){
+                    cateoria = "Obesidade";
+                }
         }
     }
 
